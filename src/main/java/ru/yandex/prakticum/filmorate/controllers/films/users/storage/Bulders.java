3e56c4636cced6b73fff10d190fa4904569551c2
EAnchugov <bergy@yandex.ru>
2022-09-26 22:10:19 +0300

    Исправлена часть ошибок

--- src/main/java/ru/yandex/prakticum/filmorate/controllers/films/users/storage/Bulders.java
+++ src/main/java/ru/yandex/prakticum/filmorate/controllers/films/users/storage/Bulders.java
@@ -1,5 +1,12 @@
 package ru.yandex.prakticum.filmorate.controllers.films.users.storage;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class Bulders {
     public void filmBuilder(){};
 }
